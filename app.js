98a8baa4a0bd4ced002e534f4ce590f113fbd691
Unknown <thanakrit.lee.2014@gmail.com>
2018-03-22 23:11:58 +1100

    Initial setup of base root express server.

--- app.js
+++ app.js
@@ -7,3 +7,21 @@ const cors = require('cors');
 app.use(cors());
 
 // Middleware for bodyparsing using both json and urlencoding.
+app.use(bodyParser.urlencoded({  extended: true }));
+app.use(bodyParser.json());
+
+// Using 3000 as the port to listen to.
+const port = 3000;
+
+app.get('/', (req, res) => {
+    /**
+     * The root directory of the server.
+     * Send back 'Root' because the directory shouldn't be available for use.
+     */
+    res.send('Root');
+});
+
+// Listen on port = 3000.
+app.listen(port, () => {
+   console.log(`Starting the server at port ${port}`);
+});
