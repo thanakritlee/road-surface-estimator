883f084b7c7db3fdfb49e3bc2d2fb3df9d4c56c1
Thanakrit Lee <thanakrit.lee.2014@gmail.com>
2018-04-06 13:47:24 +1000

    Add buttons onClick functions.

--- rse-angular-project/src/app/user-map/map-options/map-options.component.ts
+++ rse-angular-project/src/app/user-map/map-options/map-options.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  EventEmitter,
+  Output,
+  Input
+} from '@angular/core';
 
 @Component({
   selector: 'app-map-options',
@@ -7,9 +13,70 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapOptionsComponent implements OnInit {
 
-  constructor() { }
+  @Output() roadMapViewClicked = new EventEmitter < boolean > ();
+  @Output() satelliteViewClicked = new EventEmitter < boolean > ();
 
-  ngOnInit() {
+  // boolean for displaying or hidding the coordinate input options.
+  useCoordinates: boolean = false;
+
+  coordinatesButtonText: string = "Use Coordinates instead";
+
+  longitudeInput: number;
+  latitudeInput: number;
+  
+  @Input() latitudeMarker: number;
+  @Input() longitudeMarker: number;
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  onClickRoadMapView() {
+    /**
+     * Change Google Maps type to roadmap view.
+     * 
+     * Emits the roadmap event.
+     */
+    this.roadMapViewClicked.emit(true);
+    console.log('Event Emit from map-option: roadmap');
   }
 
+  onClickSatelliteView() {
+    /**
+     * Change Google Maps type to sattelite view.
+     * 
+     * Emits the satellite event.
+     */
+    this.satelliteViewClicked.emit(true);
+    console.log('Event Emit from map-option: satellite');
+  }
+
+  onClickCalculate() {
+    console.log('Calculate Area...');
+    if (this.useCoordinates) {
+      console.log(this.latitudeInput);
+      console.log(this.longitudeInput);
+    } else {
+      console.log(this.latitudeMarker);
+      console.log(this.longitudeMarker);
+    }
+  }
+
+  onClickUseCoordinates() {
+    this.useCoordinates = !this.useCoordinates;
+    this.coordinatesButtonText = this.useCoordinates ? "Use Map Marker instead" : "Use Coordinates instead";
+  }
+
+  onClickClearCoordinates() {
+    console.log('Clear coordinates');
+    this.latitudeInput = undefined;
+    this.longitudeInput = undefined;
+  }
+
+  onClickShowOnMap() {
+    /**
+     * Emit the latitudeInput and longitudeInput data to the parent component, eg. app-user-map.
+     */
+    console.log('Show on map');
+  }
 }
--- rse-angular-project/src/app/user-map/user-map.component.ts
+++ rse-angular-project/src/app/user-map/user-map.component.ts
@@ -7,12 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserMapComponent implements OnInit {
 
+  // Default/Starting coordinates on Google Maps.
+  lat: number = 51.678418;
+  lng: number = 7.809007;
+
+  // Initialising Google Maps mapTypeId.
+  mapTypeId: string = "roadmap";
+
   constructor() { }
 
   ngOnInit() {
   }
 
-  lat: number = 51.678418;
-  lng: number = 7.809007;
+  onRoadMapViewClicked(bool: boolean) {
+    this.mapTypeId = "roadmap";
+    console.log('Received event in user-map: roadmap');
+  }
+
+  onSatelliteViewClicked(bool: boolean) {
+    this.mapTypeId = "satellite";
+    console.log('Received event in user-map: satellite');
+  }
+
+  getCoordinates(coordinates: {coords: {lat: number, lng: number}}) {
+    /**
+     * Get Google Maps latitutde/longitude coordinates from mouse click
+     * event on the map.
+     * Change marker location to new coordinates from mouse click event.
+     * Change map center to new coordinates from mouse click event.
+     */
+    console.log('Map clicked');
+    console.log(coordinates.coords.lat);
+    console.log(coordinates.coords.lng);
+    this.lat = coordinates.coords.lat;
+    this.lng = coordinates.coords.lng;
+  }
 
 }
