a3c308d487434da534b6f3e70fe977dda888ca0e
Thanakrit Lee <thanakrit.lee.2014@gmail.com>
2018-04-07 08:43:08 +1000

    Add components.

--- rse-angular-project/src/app/app.module.ts
+++ rse-angular-project/src/app/app.module.ts
@@ -6,13 +6,29 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { AgmCoreModule } from '@agm/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 import { ServerMapComponent } from './server-map/server-map.component';
 import { UserMapComponent } from './user-map/user-map.component';
 import { MapOptionsComponent } from './user-map/map-options/map-options.component';
+import { HomeComponent } from './home/home.component';
+import { CalculateComponent } from './calculate/calculate.component';
+import { ToolsComponent } from './tools/tools.component';
+import { ReferencesComponent } from './references/references.component';
+import { AboutComponent } from './about/about.component';
 
+
+// Application URL routes.
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'calculate', component: CalculateComponent },
+  { path: 'tools', component: ToolsComponent },
+  { path: 'references', component: ReferencesComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
+];
 
 
 @NgModule({
@@ -21,7 +37,12 @@ import { MapOptionsComponent } from './user-map/map-options/map-options.componen
     NavigationBarComponent,
     ServerMapComponent,
     UserMapComponent,
-    MapOptionsComponent
+    MapOptionsComponent,
+    HomeComponent,
+    CalculateComponent,
+    ToolsComponent,
+    ReferencesComponent,
+    AboutComponent,
   ],
   imports: [
     BrowserModule,
@@ -30,7 +51,8 @@ import { MapOptionsComponent } from './user-map/map-options/map-options.componen
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyD5blFhmJvoOhORIYKX0JjKo5V8Vhbpq2s'
     }),
-    MDBBootstrapModule.forRoot()
+    MDBBootstrapModule.forRoot(),
+    RouterModule.forRoot(appRoutes)
   ],
   schemas: [ NO_ERRORS_SCHEMA ],
   providers: [],
